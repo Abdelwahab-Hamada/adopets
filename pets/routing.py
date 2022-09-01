f831091264187f78ab6477668c94505f65fdd566
abdelwahab youssef <abdelwahab.hamada@gmail.com>
2022-09-01 09:51:07 +0200

    cling

--- pets/routing.py
+++ pets/routing.py
@@ -1,10 +1,15 @@
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter,URLRouter
 from django.urls import path
 from .tokenAuthMiddleware import TokenAuthMiddleware
 from .consumers import Consumer
+from dj_static import Cling
 
+
+django_asgi_app = Cling(get_asgi_application())
 
 application = ProtocolTypeRouter({
+    "http": django_asgi_app,
     "websocket": TokenAuthMiddleware(URLRouter([
         path("pets/", Consumer.as_asgi()),
     ]))
