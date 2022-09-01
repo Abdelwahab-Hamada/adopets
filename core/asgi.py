4526a8ffb40ade2bae64500ee755873447f76aa0
abdelwahab youssef <abdelwahab.hamada@gmail.com>
2022-09-01 14:35:36 +0200

    online isa1

--- core/asgi.py
+++ core/asgi.py
@@ -5,7 +5,7 @@ from channels.routing import get_default_application
 from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
+django.setup()
 
 application=Cling(get_default_application())
 
