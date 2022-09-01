251eb28aaf4bad8e95704faf4fc65ecb31ad074e
abdelwahab youssef <abdelwahab.hamada@gmail.com>
2022-09-01 11:49:20 +0200

    def

--- core/asgi.py
+++ core/asgi.py
@@ -2,10 +2,11 @@ import os
 import django
 
 from pets.routing import application
+from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 django.setup()
 
-application=application
+application=get_default_application()
 
 # application = get_asgi_application()
